cd346bca7aa4b4adbcbf9d6148af006dd7b63f20
Jamie Sullivan <jamiesullivan@jamies-mbp.lan>
2020-05-08 07:17:25 -0400

    MVP

--- GitHubCard/index.js
+++ GitHubCard/index.js
@@ -3,12 +3,9 @@
            https://api.github.com/users/<your name>
 */
 
-axios .get('https://api.github.com/users/jme-sull').then(response => {
-  //console.log(response);
-  const entries = Object.entries(response.data);
-  console.log(entries);
-  document.querySelector('.cards').append(createCard(entries)); 
-});
+axios.get('https://api.github.com/users/jme-sull').then(response => {
+  console.log(response)
+//})
 
 
 /* Step 2: Inspect and study the data coming back, this is YOUR 
@@ -32,7 +29,7 @@ axios .get('https://api.github.com/users/jme-sull').then(response => {
           user, and adding that card to the DOM.
 */
 
-const followersArray = [];
+
 
 /* Step 3: Create a function that accepts a single object as its only argument,
           Using DOM methods and properties, create a component that will return the following DOM element:
@@ -54,68 +51,99 @@ const followersArray = [];
 
 */
 
-function createCard(object){
-
-  //set variables
-
-  const card = document.createElement('div').
-        userImage = document.createElement('img'),
-        cardInfo = document.createElement('div'),
-        userNameHeader = document.createElement('h3'),
-        userNameP = document.createElement('p'),
-        userLocation = document.createElement('p'),
-        userProfile = document.createElement('p'),
-        userGitHubLink = document.createElement('a'),
-        userFollowers = document.createElement('p'),
-        userFollowing = document.createElement('p'),
-        userBio = document.createElement('p');
-
-//create structure 
-
-    card.appendChild(userImage);
-    card.appendChild(cardInfo);
-    cardInfo.appendChild(userNameHeader);
-    cardInfo.appendChild(userNameP);
-    cardInfo.appendChild(userLocation);
-    cardInfo.appendChild(userProfile);
-    userProfile.appendChild(userGitHubLink);
-    cardInfo.appendChild(userFollowers);
-    cardInfo.appendChild(userFollowing);
-    cardInfo.appendChild(userBio);
-
-//add classes 
-
-    card.classList.add('card');
-    cardInfo.classList.add('card-info');
-    userNameHeader.classList.add('name');
-    userNameP.classList.add('username');
-
-//add data 
-
-    userImage.setAttribute('src', `${object.data.avatar_url}`);
-    userNameHeader.textContent = `${object.data.name}`; 
-    userNameP.textContent = `${object.data.login}`;
-    userLocation.textContent = `${object.data.location}`;
-    userGitHubLink.setAttribute('href', `${object.data.html_url}`);
-    userFollowers.textContent = `${object.data.followers}`;
-    userFollowing.textContent = `${object.data.following}`; 
-    userBio.textContent = `${object.data.bio}`;
 
 
-return card;
-        
+function createCard (data) {
+
+  const { avatar_url, name, login, location, followers, following, bio, html_url } = data
+
+  //create variables
+  card = document.createElement('div')
+  userImage = document.createElement('img')
+  cardInfo = document.createElement('div')
+  fullName = document.createElement('h3')
+  userName = document.createElement('p')
+  userLocation = document.createElement('p')
+  userProfile = document.createElement('p')
+  userProfileA = document.createElement('a')
+  followersCount = document.createElement('p')
+  followingCount = document.createElement('p')
+  userBio = document.createElement('p')
+
+
+  //append
+  card.appendChild(userImage)
+  card.appendChild(cardInfo)
+  cardInfo.appendChild(fullName)
+  cardInfo.appendChild(userName)
+  cardInfo.appendChild(userLocation)
+  cardInfo.appendChild(userProfile)
+  userProfile.appendChild(userProfileA)
+  cardInfo.appendChild(followersCount)
+  cardInfo.appendChild(followingCount)
+  cardInfo.appendChild(userBio)
+  
+  //add classes
+
+  card.classList.add('card')
+  cardInfo.classList.add('card-info')
+  fullName.classList.add('name')
+  userName.classList.add('username')
+
+
+  //inject data
+
+  userImage.src = avatar_url
+  userProfileA.href = html_url
+  fullName.textContent = `Name: ${name}`
+  userName.textContent = login
+  userLocation.textContent = `Location: ${location}`
+  followersCount.textContent = `Followers: ${followers}`
+  followingCount.textContent = `Following: ${following}`
+  userProfileA.textContent = `Profile: ${html_url}`
+  userBio.textContent = `Bio: ${bio}`
+
+ 
+
+  
+
+
+
+
+
+
+
+
+
+  console.log(card)
+
+
+
+  
+
+return card
+
 }
 
 
 
 
-/* List of LS Instructors Github username's: 
-  tetondan
-  dustinmyers
-  justsml
-  luishrd
-  bigknell
-*/
+const followersArray = ['Brimes7', 'lex-marie790', 'MileyWright', 'Scott-Crawshaw', 'JoshHill15'];
 
+followersArray.forEach(user => {
+  axios.get(`https://api.github.com/users/${user}`).then(response => {
+    const dataObject = response.data
+    const newCard = createCard(dataObject)
+    const entryPoint = document.querySelector('.cards')
+    entryPoint.appendChild(newCard)
+  })
+})
 
- document.querySelector('.cards').append(createCard(response));
+axios.get('https://api.github.com/users/jme-sull').then(response => {
+  const myData = response.data
+  const myCard = createCard(myData)
+  const entryPoint = document.querySelector('.cards')
+  entryPoint.appendChild(myCard)
+   })
+
+})
